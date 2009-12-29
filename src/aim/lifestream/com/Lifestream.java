6f466d4b994e8e12caa207c15dfaa5a52780ec4a
GregBC <gbcypes@gmail.com>
2009-12-29 09:47:37 -0500

    Added UI handlers

--- src/aim/lifestream/com/Lifestream.java
+++ src/aim/lifestream/com/Lifestream.java
@@ -4,8 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Button;
+import android.text.Editable;
 import android.view.View;
 
 import java.util.*;
@@ -33,66 +35,21 @@ public class Lifestream extends Activity {
     public void SignOn()
     {
 		try {
-	        // Construct data
-	        String data = URLEncoder.encode("clientName", "UTF-8") + "=" + URLEncoder.encode("aimStatusTest", "UTF-8");
-	        data += "&" + URLEncoder.encode("clientVersion", "UTF-8") + "=" + URLEncoder.encode("1.0.0.0", "UTF-8");
-	        data += "&" + URLEncoder.encode("devId", "UTF-8") + "=" + URLEncoder.encode("ai1iu803FncGRd61", "UTF-8");
-	        data += "&" + URLEncoder.encode("f", "UTF-8") + "=" + URLEncoder.encode("qs", "UTF-8");
-	        data += "&" + URLEncoder.encode("pwd", "UTF-8") + "=" + URLEncoder.encode("maryland", "UTF-8");
-	        data += "&" + URLEncoder.encode("r", "UTF-8") + "=" + URLEncoder.encode("0", "UTF-8");
-	        data += "&" + URLEncoder.encode("s", "UTF-8") + "=" + URLEncoder.encode("gregtest6", "UTF-8");
-	        data += "&" + URLEncoder.encode("tokenType", "UTF-8") + "=" + URLEncoder.encode("longterm", "UTF-8");
-	        
-	        // Send data
-	        URL url = new URL("https://api.screenname.aol.com/auth/clientLogin");
-	        URLConnection conn = url.openConnection();
-	        conn.setDoOutput(true);
-	        OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
-	        wr.write(data);
-	        wr.flush();
-	    
-	        // Get the response
-	        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-	        String line;
-	        String params[];
-	        String delimeter = "&";
-	        while ((line = rd.readLine()) != null) {
-	        	if (line.contains("statusCode=200"))
-	        	{
-	        		params = line.split(delimeter);
-	        		Map<String, String> map = new HashMap<String, String>();
-	        		for(int i =0; i < params.length ; i++)
-	        		{
-	        			String str = params[i];
-	        			int equal = str.indexOf("=");
-	        			String key = str.substring(0, equal);
-	        			equal ++;
-	        			String val = str.substring(equal);
-	        			map.put(key, val);
-	        		}
-	        		
-	        	    TextView tv = new TextView(this);
-	        	    tv.setText("Client Login Success");
-	        	    setContentView(tv);
-	        	    StartSession(map, "maryland");
-	        	}
-	        	else
-	        	{
-	        		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-	        		builder.setMessage("There was an error authenticating")
-	        		       .setCancelable(false)
-	        		       .setNegativeButton("OK", new DialogInterface.OnClickListener() {
-	        		           public void onClick(DialogInterface dialog, int id) {
-	        		                dialog.cancel();
-	        		           }
-	        		       });
-	        		AlertDialog alert = builder.create();
-	        		alert.show();
-	        	}
-	        }
-	        wr.close();
-	        rd.close();
-		} catch (IOException e) {
+			AimApi aim = new AimApi();
+			EditText uname = (EditText)findViewById(R.id.entry);
+			String username = uname.getText().toString();
+			EditText pw = (EditText)findViewById(R.id.pwd);
+			String password = pw.getText().toString();
+			setContentView(R.layout.connect);
+			if (aim.SignOn(username, password))
+			{
+				aim.StartSession();
+			}
+			else
+			{
+				
+			}
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
